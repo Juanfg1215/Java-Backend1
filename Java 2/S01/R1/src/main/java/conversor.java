bfc9537edf2684c198e8ac64fe6e2a9e5769ba9f
Juanfg1215 <119272626+Juanfg1215@users.noreply.github.com>
2023-02-28 13:13:34 -0600

    Delete Java 2/S01 directory

--- Java 2/S01/R1/src/main/java/conversor.java
+++
@@ -1,9 +0,0 @@
-public class conversor {
-    public static void main (String [] args){
-        int number = 10;
-
-        String binary = Integer.toBinaryString(number);
-
-        System.out.println ("El numero entero " + number + " en binario es: " + binary);
-    }
-}
